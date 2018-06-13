051c964c8ccf3ac905fe5f0b7353fe8c6e5af675
shoxxstraights <40190658+shoxxstraights@users.noreply.github.com>
2018-06-13 07:07:47 +0200

    Update CreatingaHut.sol

--- CreatingaHut.sol
+++ CreatingaHut.sol
@@ -1,7 +1,8 @@
 /// @title Base contract for creating a hut. Holds all common structs, events and base variables.
-/// @dev See the KittyCore contract documentation to understand how the various contract facets are arranged.
+
 
 contract CreatingaHut {
+
     /*** EVENTS ***/
 
     ///  The Construction event is fired whenever a new hut comes into existence on the blockchain.
@@ -46,112 +47,64 @@ contract CreatingaHut {
     //  Used internally inside balanceOf() to resolve ownership count.
     mapping (address => uint256) ownershipTokenCount;
 
-    /// @dev A mapping from KittyIDs to an address that has been approved to call
-    ///  transferFrom(). Each Kitty can only have one approved address for transfer
+    ///  A mapping from HutIDs to an address that has been approved to call
+    ///  transferFrom(). Each hut can only have one approved address for transfer
     ///  at any time. A zero value means no approval is outstanding.
     mapping (uint256 => address) public hutIndexToApproved;
 
-    /// @dev A mapping from KittyIDs to an address that has been approved to use
-    ///  this Kitty for siring via breedWith(). Each Kitty can only have one approved
-    ///  address for siring at any time. A zero value means no approval is outstanding.
-    mapping (uint256 => address) public sireAllowedToAddress;
 
-    /// @dev The address of the ClockAuction contract that handles sales of Kitties. This
-    ///  same contract handles both peer-to-peer sales as well as the gen0 sales which are
-    ///  initiated every 15 minutes.
-    SaleClockAuction public saleAuction;
+    ///  The address of the ClockAuction contract that handles sales of huts. 
+    SaleSellingaHut public saleAuction;
 
-    /// @dev The address of a custom ClockAuction subclassed contract that handles siring
-    ///  auctions. Needs to be separate from saleAuction because the actions taken on success
-    ///  after a sales and siring auction are quite different.
-    SiringClockAuction public siringAuction;
+   
 
-    /// @dev Assigns ownership of a specific Kitty to an address.
+    ///  Assigns ownership of a specific Hut to an address.
     function _transfer(address _from, address _to, uint256 _tokenId) internal {
-        // Since the number of kittens is capped to 2^32 we can't overflow this
-        ownershipTokenCount[_to]++;
+        
         // transfer ownership
-        kittyIndexToOwner[_tokenId] = _to;
-        // When creating new kittens _from is 0x0, but we can't account that address.
-        if (_from != address(0)) {
-            ownershipTokenCount[_from]--;
-            // once the kitten is transferred also clear sire allowances
-            delete sireAllowedToAddress[_tokenId];
-            // clear any previously approved ownership exchange
-            delete kittyIndexToApproved[_tokenId];
+        hutIndexToOwner[_tokenId] = _to;
+        
         }
         // Emit the transfer event.
         Transfer(_from, _to, _tokenId);
     }
 
-    /// @dev An internal method that creates a new kitty and stores it. This
+    ///  An public method that creates a new kitty and stores it. This
     ///  method doesn't do any checking and should only be called when the
-    ///  input data is known to be valid. Will generate both a Birth event
+    ///  input data is known to be valid. Will generate both a Construction event
     ///  and a Transfer event.
-    /// @param _matronId The kitty ID of the matron of this cat (zero for gen0)
-    /// @param _sireId The kitty ID of the sire of this cat (zero for gen0)
-    /// @param _generation The generation number of this cat, must be computed by caller.
-    /// @param _genes The kitty's genetic code.
-    /// @param _owner The inital owner of this cat, must be non-zero (except for the unKitty, ID 0)
-    function _createKitty(
-        uint256 _matronId,
-        uint256 _sireId,
-        uint256 _generation,
-        uint256 _genes,
+    /// @param _deedNumber Derived from original copy of title deed.
+    /// @param _standNumber Also derived from the original title deed.
+    /// @param _owner The inital owner of this cat, must be non-zero.
+    function _createHut(
+        uint256 _deedNumber,
+        uint256 _standNumber,
         address _owner
     )
-        internal
-        returns (uint)
-    {
-        // These requires are not strictly necessary, our calling code should make
-        // sure that these conditions are never broken. However! _createKitty() is already
-        // an expensive call (for storage), and it doesn't hurt to be especially careful
-        // to ensure our data structures are always valid.
-        require(_matronId == uint256(uint32(_matronId)));
-        require(_sireId == uint256(uint32(_sireId)));
-        require(_generation == uint256(uint16(_generation)));
-
-        // New kitty starts with the same cooldown as parent gen/2
-        uint16 cooldownIndex = uint16(_generation / 2);
-        if (cooldownIndex > 13) {
-            cooldownIndex = 13;
-        }
+        public
+        returns (uint);
+   
 
         Kitty memory _kitty = Kitty({
-            genes: _genes,
-            birthTime: uint64(now),
-            cooldownEndBlock: 0,
-            matronId: uint32(_matronId),
-            sireId: uint32(_sireId),
-            siringWithId: 0,
-            cooldownIndex: cooldownIndex,
-            generation: uint16(_generation)
+            constructionTime: uint64(now),
+            deedNumber: uint256(_deedNumber),
+            standNumber: uint256(_standNumber),
         });
-        uint256 newKittenId = kitties.push(_kitty) - 1;
-
-        // It's probably never going to happen, 4 billion cats is A LOT, but
-        // let's just be 100% sure we never let this happen.
-        require(newKittenId == uint256(uint32(newKittenId)));
+        uint256 newhutId = huts.push(_hut) - 1;
 
         // emit the birth event
         Birth(
             _owner,
-            newKittenId,
-            uint256(_kitty.matronId),
-            uint256(_kitty.sireId),
-            _kitty.genes
+            newhutId,
+            uint256(_hut.deedNumber),
+            uint256(_hut.standNumber),
         );
 
         // This will assign ownership, and also emit the Transfer event as
         // per ERC721 draft
-        _transfer(0, _owner, newKittenId);
+        _transfer(0, _owner, newhutId);
 
-        return newKittenId;
+        return newhutId;
     }
 
-    // Any C-level can fix how many seconds per blocks are currently observed.
-    function setSecondsPerBlock(uint256 secs) external onlyCLevel {
-        require(secs < cooldowns[0]);
-        secondsPerBlock = secs;
-    }
 }
