c695c0fa71afc23e9154c277c651dd5be21e39ec
shoxxstraights <40190658+shoxxstraights@users.noreply.github.com>
2018-06-15 07:24:18 +0200

    Update CreatingaHut.sol

--- CreatingaHut.sol
+++ CreatingaHut.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.4.24;
 import "./ERC721Basic.sol";
 
 
-/// @title Base contract for creating a hut. Holds all common structs, events and base variables.
-/// hut symbolizes digital deed.
+/// @title CreatingaHut.sol. Holds all common structs, events and base variables
+/// for the construction of huts on the blockchain.
 
 contract CreatingaHut {
 
