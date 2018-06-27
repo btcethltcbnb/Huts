86d4e5840a7200878a79fa231e5f368e5316c43f
shoxxstraights <40190658+shoxxstraights@users.noreply.github.com>
2018-06-27 23:41:26 +0200

    Update ERC721Basic.sol

--- ERC721Basic.sol
+++ ERC721Basic.sol
@@ -7,7 +7,7 @@ import "./ERC165.sol";
  /// @title ERC721 Non-Fungible Token Standard basic interface
  /// See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
 
-contract ERC721Basic {
+contract ERC721 {
   event Transfer(
     address indexed _from,
     address indexed _to,
